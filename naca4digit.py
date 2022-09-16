00c2bd43c10a39b93e26c7c7c1df0e5b081b508e
Brandon <denbjornen505@gmail.com>
2022-09-15 20:01:05 -0600

    Added support for symmetric 4-digit profiles.

--- naca4digit.py
+++ naca4digit.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 class NACA4(object):
 
     def __init__(self, digits, points=50, path=None, method='linear', save=False):
@@ -26,7 +27,6 @@ class NACA4(object):
         elif x > p:
             return np.arctan((2*m/(1-p)**2)*(p-x))
 
-
     def camber(self, x, m, p):
 
         if x <= p:
@@ -34,19 +34,20 @@ class NACA4(object):
         elif x > p:
             return (m/(1-p)**2)*((1-2*p)+2*p*x-x**2)
 
-
     def thickness(self, x, t):
 
         return 5*t*(0.2969*np.sqrt(x) - 0.126*x - 0.3516*x**2 + 0.2843*x**3 - 0.1015*x**4)
-
 
     def foil(self, x, m, p, t, upper=True):
 
         d = 1 if upper else -1
 
-        return x - d*self.thickness(x, t)*np.sin(self.theta(x, m, p)), \
-               self.camber(x, m, p) + d*self.thickness(x, t)*np.cos(self.theta(x, m, p)), 0.0
+        if m == 0 and p == 0:
+            return x, d*5*t*(0.2969*np.sqrt(x) - 0.1260*x - 0.3516*x**2 + 0.2843 * x**3 - 0.1015*x**4), 0.0
 
+        else:
+            return x - d*self.thickness(x, t)*np.sin(self.theta(x, m, p)), \
+                   self.camber(x, m, p) + d*self.thickness(x, t)*np.cos(self.theta(x, m, p)), 0.0
 
     def foil_discret(self, p, points, method='linear'):
 
@@ -63,7 +64,6 @@ class NACA4(object):
 
         return x
 
-
     def main(self, digits, points, path, method, save):
         m = float(digits[0])/100
         p = float(digits[1])/10
@@ -76,6 +76,10 @@ class NACA4(object):
         # Calculate the upper and lower profiles
         upper = np.insert(np.array([self.foil(i, m, p, t) for i in x]), 0, np.array([0, 0, 0]), 0)
         lower = np.insert(np.array([self.foil(i, m, p, t, upper=False) for i in x]), 0, np.array([0, 0, 0]), 0)
+
+        plt.plot(upper[:, 0], upper[:, 1])
+        plt.plot(lower[:, 0], lower[:, 1])
+        plt.show()
 
         # Save the files
         if save:
@@ -91,6 +95,7 @@ class NACA4(object):
         self.upper, self.lower = upper, lower
         self.n_points = len(upper)+len(lower)
         self.ordered_points = np.concatenate([np.flipud(upper[1:]), lower])
+
 
 if __name__ == "__main__":
     import argparse
